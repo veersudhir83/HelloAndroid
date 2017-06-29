4141f0b1c3681dcbcd6e7c644c7aa2920d8878dc
veersudhir83 <veersudhir83@gmail.com>
2017-06-29 17:13:20 +0530

    Added GeoLocation Code

---
+++ app/src/main/java/io/sudheer/android/helloandroid/Constants.java
@@ -0,0 +1,14 @@
+package io.sudheer.android.helloandroid;
+
+/**
+ * Created by Sudheer Veeravalli on 6/29/2017.
+ */
+
+public final class Constants {
+    public static final int SUCCESS_RESULT = 0;
+    public static final int FAILURE_RESULT = 1;
+    public static final String PACKAGE_NAME = "io.sudheer.android.helloandroid";
+    public static final String RECEIVER = PACKAGE_NAME + ".RECEIVER";
+    public static final String RESULT_DATA_KEY = PACKAGE_NAME + ".RESULT_DATA_KEY";
+    public static final String LOCATION_DATA_EXTRA = PACKAGE_NAME + ".LOCATION_DATA_EXTRA";
+}
