fdebd59dd36bff5f247069e89002d83358ebe200
veersudhir83 <veersudhir83@gmail.com>
2017-07-03 14:49:54 +0530

    Changes from Office

--- app/src/main/java/io/sudheer/android/helloandroid/HomePageActivity.java
+++ app/src/main/java/io/sudheer/android/helloandroid/HomePageActivity.java
@@ -1,6 +1,5 @@
 package io.sudheer.android.helloandroid;
 
-import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
@@ -9,13 +8,12 @@ import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import java.util.List;
@@ -119,7 +117,7 @@ public class HomePageActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public boolean isGoogleMapsInstalled()
+    private boolean isGoogleMapsInstalled()
     {
         try
         {
@@ -132,7 +130,7 @@ public class HomePageActivity extends AppCompatActivity {
         }
     }
 
-    public DialogInterface.OnClickListener getGoogleMapsListener()
+    private DialogInterface.OnClickListener getGoogleMapsListener()
     {
         return new DialogInterface.OnClickListener()
         {
